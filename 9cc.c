a75bd8b1bbc899bff20381dd917eb97249578523
chicu <chc1129@gmail.com>
2019-05-30 07:31:08 +0900

    add token func

--- 9cc.c
+++ 9cc.c
@@ -10,12 +10,23 @@ enum {
   TK_EOF,       // 入力の終わりを表すトークン
 };
 
+enum {
+  ND_NUM = 256, // 整数のノードの型
+};
+
 // トークンの型
 typedef struct {
   int ty;       // トークンの型
   int val;      // tyがTK_NUMの場合,その数値
   char *input;  // トークン文字列(エラーメッセージ用)
 } Token;
+
+typedef struct Node {
+  int ty;            // 演算子かND_NUM
+  struct Node *lhs;  // 左辺
+  struct Node *rhs;  // 右辺
+  int val;           // tyがND_NUMの場合のみ使う
+} Node;
 
 // 入力プログラム
 char *user_input;
@@ -79,6 +90,99 @@ void tokenize() {
   tokens[i].input = p;
 }
 
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+int consume(int ty) {
+  if (tokens[pos].ty != ty)
+    return 0;
+  pos++;
+  return 1;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node('+', node, mul());
+    else if (consume('-')
+      node = new_node('-', node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = term();
+
+  for (++) {
+    if (consume('*'))
+      node = new_node('*', node, term());
+    else if (consume('/'))
+      node = new_node('/', node, term());
+    else
+      return node;
+  }
+}
+
+Node *term() {
+  // 次のトークンが'('なら,"(" exp ")"のはず
+  if (consume('(')) {
+    Node *node = expr();
+    if (!consume(')'))
+      error_at(tokens[pos].input, "開きカッコに対応する閉じカッコがありません");
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  if (tokens[pos].ty == TK_NUM)
+    return new_node_num(tokens[pos++].val);
+
+  error_at(tokens[pos].input, "数値でも開きカッコでもないトークンです");
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  imul rdi\n");
+    break;
+  case '/':
+    printf("  cpa\n");
+    printf("  idiv rdi\n");
+  }
+
+  printf("  push rax\n");
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
